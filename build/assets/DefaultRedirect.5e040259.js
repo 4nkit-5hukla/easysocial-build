42f7f7f2e5b1eb56465da160a6d03b0147f13e5d
Ankit Shukla <ankit5hukla@hotmail.com>
2022-12-06 10:55:30 +0530

    Signed-off-by: Ankit Shukla <ankit5hukla@hotmail.com>

---
+++ build/assets/DefaultRedirect.5e040259.js
@@ -0,0 +1,1 @@
+import{j as a,N as t}from"./index.3120fd12.js";const s=({to:e})=>a(t,{to:e,replace:!0});export{s as default};
--- build/assets/styled.00f71db4.js
+++
@@ -1,1 +0,0 @@
-import{Z as t}from"./index.7f82941f.js";const s=t(),o=s;export{o as s};
